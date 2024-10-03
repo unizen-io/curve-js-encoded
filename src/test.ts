1f84af0432fa836af6bcf0ef2daadcd1d21ebac4
kunizen <kien@unizen.io>
2024-10-03 15:25:58 +0700

    test: print memory usage

--- src/test.ts
+++ src/test.ts
@@ -1,32 +1,35 @@
-import { resolve } from 'path';
-import curve from './index'
-import {ICalldata} from './interfaces'
+import curve from "./index";
+import { ICalldata } from "./interfaces";
 
+// const rpc = "https://arb1.arbitrum.io/rpc";
+// const chainId = 42161;
+// const srcTokenAddress = "0xca5ca9083702c56b481d1eec86f1776fdbd2e594"; // rsr polygon
+// const dstTokenAddress = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1"; // weth polygon
+// const dstTokenAddress = "0xaf88d065e77c8cc2239327c5edb3a432268e5831"; // usdc polygon
 const rpc = "https://ancient-dry-friday.matic.quiknode.pro/8ff6b30f0829e4bded452e60d0bcf9ac712002db/";
 const chainId = 137;
 const srcTokenAddress = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'; // usdc polygon
 const dstTokenAddress = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F'; // usdt
-const amountIn = '10';
+// const srcTokenAddress = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'; // wmatic polygon
+// const dstTokenAddress = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'; // usdc polygon
+const amountIn = "10";
 
 export const sleep = async (time: number) => {
     return new Promise<void>((resolve, reject) => {
         setTimeout(() => {
             resolve();
-        }, time)
-    })
-}
+        }, time);
+    });
+};
 
 // To run this test:
 // nvm use v18.16.0
 // npx ts-node --project tsconfig.build.json src/test.ts
 export const main = async () => {
-    console.log('start')
-    const start0 = new Date().getTime()
+    console.log("start");
+    const start0 = new Date().getTime();
     await curve.init("JsonRpc", { url: rpc }, { gasPrice: 0, maxFeePerGas: 0, maxPriorityFeePerGas: 0, chainId });
-    const in0 = new Date().getTime()
     await curve.factory.fetchPools(true);
-    const in1 = new Date().getTime()
-    console.log("one factory", in1-in0, 'ms')
     await curve.crvUSDFactory.fetchPools(true);
     await curve.EYWAFactory.fetchPools(true);
     await curve.cryptoFactory.fetchPools(true);
@@ -34,26 +37,21 @@ export const main = async () => {
     await curve.tricryptoFactory.fetchPools(true);
     await curve.stableNgFactory.fetchPools(true);
     await curve.getAmplificationCoefficientsFromApi();
-    console.log('factory pool list',curve.factory.getPoolList().length)
-    const start = new Date().getTime()
-    const bestRouteAndOutput = await curve.router.getBestRouteAndOutput(
-        srcTokenAddress,
-        dstTokenAddress,
-        amountIn
+    console.log("factory pool list", curve.factory.getPoolList().length);
+    const start = new Date().getTime();
+    const bestRouteAndOutput = await curve.router.getBestRouteAndOutput(srcTokenAddress, dstTokenAddress, amountIn);
+    const end = new Date().getTime();
+    console.log("fetch time", start - start0, "ms");
+    console.log("total time", end - start, "ms");
+    console.log(
+        "bestRouteAndOutput",
+        bestRouteAndOutput.route.map((item) => item.inputCoinAddress)
     );
-    const end = new Date().getTime()
-    console.log("fetch time", start-start0, 'ms')
-    console.log("total time", end-start, 'ms')
-    console.log('bestRouteAndOutput', bestRouteAndOutput.route.map((item)=>item.inputCoinAddress))
-    const swapTx = (await curve.router.swap(
-        srcTokenAddress,
-        dstTokenAddress,
-        amountIn,
-        0.01,
-        true
-    )) as ICalldata; 
-    console.log('quote', swapTx.actualQuote)
-
+    const swapTx = (await curve.router.swap(srcTokenAddress, dstTokenAddress, amountIn, 0.01, true)) as ICalldata;
+    console.log("quote", swapTx.actualQuote);
+    for (const [key,value] of Object.entries(process.memoryUsage())){
+        console.log(`Memory usage by ${key}, ${value/1000000}MB `)
+    }
     // await sleep(60000*6);
     // console.log("Start get route second time")
     // const start2 = new Date().getTime()
@@ -70,7 +68,7 @@ export const main = async () => {
     //     amountIn,
     //     0.01,
     //     true
-    // )) as ICalldata; 
+    // )) as ICalldata;
     // console.log('quote', swapTx1.actualQuote)
 };
 
